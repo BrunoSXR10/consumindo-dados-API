451df179e349998d66208f9a8a3b28738ced5b2f
bruno <brunoez2002@gmail.com>
2022-09-12 11:13:26 -0300

    CEP inválido

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
 async function buscaEndereco(cep){
+    let mensagemErro = document.getElementById("erro")
+    mensagemErro.innerHTML = ""
     try {
         let consultaCEP = await fetch(`https://viacep.com.br/ws/${cep}/json/`);
         let consultaCEPNew = await consultaCEP.json();
@@ -6,19 +8,26 @@ async function buscaEndereco(cep){
         if(consultaCEPNew.erro) {
             throw Error('CEP não existe')
         }
+
+        let cidade = document.getElementById('cidade')
+        let estado = document.getElementById('estado')
+        let endereco = document.getElementById('endereco')
+
+        cidade.value = consultaCEPNew.localidade
+        estado.value = consultaCEPNew.uf
+        endereco.value = consultaCEPNew.logradouro
+
+
         console.log(consultaCEPNew);
         return consultaCEPNew
     } catch (erro) {
+        mensagemErro.innerHTML = `<p>CEP não identificado aaa</p>`
         console.log(erro)
     }
 }
 
 let cep = document.getElementById('cep')
 cep.addEventListener('focusout', () => buscaEndereco(cep.value));
-
-buscaEndereco();
-
-
 
 
 
